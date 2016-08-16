c9ee35d3fc57be9f9d5900afc45c1ebc0b48a242
SaSaChu <teresa7397@gmail.com>
2016-08-16 19:26:20 +0800

    完成聯絡我們，vision

--- js/public.js
+++ js/public.js
@@ -6,13 +6,27 @@
 $(function () {
    $('.banner, .spb, .peopimg').imgLiquid ({verticalAlign: 'center'});
 
+
    $('.navboxout, #navbox .x').click(function() {
-    $('#navbox').toggleClass('sn');
+     $('#navbox').toggleClass('sn');
      $('#cover').toggleClass('show');
-   })
+   });
+
 
    $('.openlang').click(function() {
      $('.lang').toggleClass('langshow');
-   })
+   });
+
+
+   $('.ba').click(function() {
+    $('.ba').removeClass ('h');
+    $(this).addClass ('h');
+
+    $('.partnersbox').removeClass('partnershow');
+    $('.partnersbox').eq($(this).index()).addClass('partnershow');
+   });
+
+   $('.ba').eq (0).click();
+
 });
 
