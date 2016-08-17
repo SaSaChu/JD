15dac8c5807b107693f6d2756762e25f9ef4b165
SaSaChu <teresa7397@gmail.com>
2016-08-17 11:10:02 +0800

    Fix html code.

--- js/public.js
+++ js/public.js
@@ -40,14 +40,43 @@ $(function () {
     }
    })
 
+  $('.serviceboxs').each (function () {
+    var $that = $(this);
+    $that.get (0).ori_h = $that.height () + 31 * 2;
+    $that.get (0).sh_h = $that.find ('.sc_detail').addClass ('B').height () + 31 * 2;
+    $that.find ('.sc_detail').removeClass ('B');
+
+    $that.find ('.sc_a03,.sc_a01,.sc_b04').click(function () {
+      if ($(this).parent().hasClass('A')) {
+        $that.css ({
+          'height': $that.get (0).sh_h
+        }).animate ({
+          'height': $that.get (0).ori_h
+        });
+
+      } else {
+        $that.css ({
+          'height': $that.get (0).ori_h
+        }).animate ({
+          'height': $that.get (0).sh_h
+        });
+
+      }
+    });
+  });
+
+  // $('.case_detail').hover(function(){
+  //   $(this).removeClass('D');
+  //   $(this).addClass('C');
+  // }, function(){
+  //  $(this).removeClass('C');
+  //  $(this).addClass('D');
+  // });
 
 
-  $('.case_detail').hover(function(){
-    $(this).removeClass('D');
-    $(this).addClass('C');
-  }, function(){
-   $(this).removeClass('C');
-   $(this).addClass('D');
+  $('.case_detail .case_a03').click(function () {
+console.log ('xxx');
+
   });
 
 
