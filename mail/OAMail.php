b890d61ab54637ead43623423bbe0276c906d07b
OA Wu <comdan66@gmail.com>
2016-08-28 18:12:16 +0800

    no message

---
+++ mail/OAMail.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+error_reporting(E_ALL ^ E_STRICT);
+
+require_once 'PHPMailer-5.2.13/class.phpmailer.php';
+require_once 'PHPMailer-5.2.13/class.smtp.php';
+
+
+class OAMail extends PHPMailer {
+
+  public function __construct () {
+    parent::__construct ();
+
+    $config = include('config.php');
+
+    if (isset ($config['host']) && isset ($config['port']) && isset ($config['user']) && isset ($config['passwd']) && isset ($config['from']) && isset ($config['from_name']) && $config['host'] && $config['port'] && $config['user'] && $config['passwd'] && $config['from'] && $config['from_name']) {
+      $this->isSMTP ();
+      $this->SMTPAuth = true;
+      $this->Host = $config['host'];
+      $this->Port = $config['port'];
+      $this->Username = $config['user'];
+      $this->Password = $config['passwd'];
+      $this->From = $config['from'];
+      $this->FromName = $config['from_name'];
+    }
+
+    $this->CharSet = $config['charset'];
+    $this->Encoding = $config['encoding'];
+    $this->isHTML (true);
+    $this->WordWrap = 50;
+
+    if (!empty($config['secure'])) {
+      $this->SMTPSecure = $config['secure'];
+    }
+  }
+
+  public function addTo ($address, $name = '') {
+    $this->addAddress ($address, $name);
+    return $this;
+  }
+  public function addCC ($address, $name = '') {
+    parent::addCC ($address, $name);
+    return $this;
+  }
+  public function addBCC ($address, $name = '') {
+    parent::addBCC ($address, $name);
+    return $this;
+  }
+  public function addFile ($path, $name = '') {
+    $this->addAttachment ($path, $name);
+    return $this;
+  }
+  public function setSubject ($subject) {
+    $this->Subject = $subject;
+    return $this;
+  }
+  
+  public function setBody ($body) {
+    $this->Body = $body;
+    return $this;
+  }
+
+  public function setFrom ($address, $name = '') {
+    parent::setFrom ($address, $name);
+    return $this;
+  }
+
+  public static function create () {
+    return new self ();
+  }
+}
---
+++ mail/test.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+require_once 'OAMail.php';
+// phpinfo ();
+//  exit ();
+
+try {
+
+$a = OAMail::create ()->addTo ('comdan66@gmail.com', 'OA')
+                 ->setSubject (date('YmdHis'))
+                 ->setBody ("<article><p>Hi 管理員,</p><p style='padding-left: 20px'>剛剛有一個訪客在您的「聯絡我們」留言囉，以下他所留下的聯絡資料：</p><table style='margin-left: 20px;border-collapse: collapse;width: calc(100% - 20px * 2);'><tbody><tr><th style='width: 100px;text-align: right;padding: 5px 5px 10px 0;border-bottom: 1px solid rgba(200, 200, 200, 1);'>稱 呼：</th><td style='text-align: left;padding: 5px 0 10px 5px;border-bottom: 1px solid rgba(200, 200, 200, 1);'>OA Wu</td></tr><tr><th style='width: 100px;text-align: right;padding: 11px 5px 10px 0;border-bottom: 1px solid rgba(200, 200, 200, 1);'>E-Mail：</th><td style='text-align: left;padding: 11px 0 10px 5px;border-bottom: 1px solid rgba(200, 200, 200, 1);'>comdan66@gmail.com</td></tr><tr><th style='width: 100px;text-align: right;padding: 11px 5px 10px 0;border-bottom: 1px solid rgba(200, 200, 200, 1);'>內容：</th><td style='text-align: left;padding: 11px 0 10px 5px;border-bottom: 1px solid rgba(200, 200, 200, 1);'>dsdasf fds fwretgesef gfdvs</td></tr></tbody></table><br/><p style='padding-left: 20px'>如果需要詳細列表，可以置<a href='' style='color: rgba(96, 156, 255, 1);margin: 0 2px;'>管理後台</a>檢閱。</p></article>")
+                 ->send ();
+
+} catch (Exception $e) {
+    echo $e->getMessage(); //Boring error messages from anything else!
+}
+ echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+ var_dump ($a);
+ exit ();
